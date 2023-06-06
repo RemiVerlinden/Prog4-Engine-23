c5ff04e021884012d000e3df73d5b326fc96ed42
Remi <Remi.Verlinden@outlook.com>
2023-06-06 19:09:12 +0200

    added error check to physicsSystem

--- Minigin/PhysicsSystem.cpp
+++ Minigin/PhysicsSystem.cpp
@@ -2,23 +2,30 @@
 #include "GameObject.h"
 #include "ColliderComponent.h"
 
+#include <cassert>
+
 bool dae::PhysicsSystem::CheckLevelCollision(GameObject* pObject) const
 {
+    assert(pObject && "GameObject pointer is null!");
     std::vector<ColliderComponent*> colliders{pObject->GetAllComponents<ColliderComponent>()};
     for (ColliderComponent* pCollider : colliders)
     {
+        assert(pCollider && "ColliderComponent pointer is null!");
         if (CheckLevelCollision(pCollider)) return true;
     };
 
-   return false;
+    return false;
 }
 
 bool dae::PhysicsSystem::CheckLevelCollision(ColliderComponent* pCollider) const
 {
-    for (ColliderComponent* pCollider : m_pColliders)
+    assert(pCollider && "ColliderComponent pointer is null!");
+    for (ColliderComponent* pOtherCollider : m_pColliders)
     {
+        assert(pOtherCollider && "ColliderComponent in m_pColliders is null!");
+
         const Box& a = pCollider->GetCollider();
-        const Box& b = pCollider->GetCollider();
+        const Box& b = pOtherCollider->GetCollider();
 
         if (CheckCollisionAABB(a, b)) return true;
     }
@@ -27,29 +34,35 @@ bool dae::PhysicsSystem::CheckLevelCollision(ColliderComponent* pCollider) const
 
 void dae::PhysicsSystem::AddCollider(ColliderComponent* pCollider)
 {
+    assert(pCollider && "ColliderComponent pointer is null!");
     m_pColliders.emplace_back(pCollider);
 }
 
 void dae::PhysicsSystem::AddCollider(GameObject* pObject)
 {
+    assert(pObject && "GameObject pointer is null!");
     std::vector<ColliderComponent*> colliders{pObject->GetAllComponents<ColliderComponent>()};
     for (ColliderComponent* pCollider : colliders)
     {
+        assert(pCollider && "ColliderComponent pointer is null!");
         AddCollider(pCollider);
     };
 }
 
 void dae::PhysicsSystem::RemoveCollider(ColliderComponent* pCollider)
 {
+    assert(pCollider && "ColliderComponent pointer is null!");
     std::erase(m_pColliders, pCollider);
 }
 
 void dae::PhysicsSystem::RemoveCollider(GameObject* pObject)
 {
+    assert(pObject && "GameObject pointer is null!");
     std::vector<ColliderComponent*> colliders{pObject->GetAllComponents<ColliderComponent>()};
 
     for (ColliderComponent* pCollider : colliders)
     {
+        assert(pCollider && "ColliderComponent pointer is null!");
         RemoveCollider(pCollider);
     }
 }
